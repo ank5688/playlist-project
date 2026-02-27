629b222e92e4ec9c38cbd7a7c1638cb4a069c356
Amelia Karamanos <ank5688@psu.edu>
2026-02-27 10:40:46 -0500

    Slide block created, previous and next buttons added and functioning, need to fix where content appears

--- playlist-project.js
+++ playlist-project.js
@@ -21,10 +21,19 @@ export class PlaylistProject extends DDDSuper(I18NMixin(LitElement)) {
   constructor() {
     super();
     this.title = "";
+    this.bodyText = "";
+    this.slides = [
+      {title: "Slide 1, subheading", bodyText: "Content for slide 1"},
+      {title: "Slide 2", bodyText: "Content for slide 2"},
+      {title: "Slide 3", bodyText: "Content for slide 3"},
+      {title: "Slide 4", bodyText: "Content for slide 4"}
+    ];
+    this.currentIndex = 0;
     this.t = this.t || {};
     this.t = {
       ...this.t,
       title: "Title",
+      bodyText: "Body text",
     };
     this.registerLocalization({
       context: this,
@@ -39,6 +48,9 @@ export class PlaylistProject extends DDDSuper(I18NMixin(LitElement)) {
     return {
       ...super.properties,
       title: { type: String },
+      bodyText: { type: String},
+      slides: { type: Array },
+      currentIndex: { type: Number }
     };
   }
 
@@ -49,7 +61,7 @@ export class PlaylistProject extends DDDSuper(I18NMixin(LitElement)) {
       :host {
         display: block;
         color: var(--ddd-theme-primary);
-        background-color: var(--ddd-theme-accent);
+        background-color: var(--ddd-theme-default-beaver70);
         font-family: var(--ddd-font-navigation);
       }
       .wrapper {
@@ -59,18 +71,47 @@ export class PlaylistProject extends DDDSuper(I18NMixin(LitElement)) {
       h3 span {
         font-size: var(--playlist-project-label-font-size, var(--ddd-font-size-s));
       }
+      .demo {
+        width: 300px;
+        padding: var(--ddd-spacing-2);
+        border: 1px solid var(--ddd-theme-primary);
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 1);
+      }
     `];
   }
 
   // Lit render the HTML
   render() {
+    const slide = this.slides[this.currentIndex];
     return html`
 <div class="wrapper">
-  <h3><span>${this.t.title}:</span> ${this.title}</h3>
+  <h3><span>${slide.title}:</span> ${this.title}</h3>
+  <p><span>${slide.bodyText}:</span> ${this.bodyText}</p>
+
+  <button @click=${this._prevSlide}
+    ?disabled="${this.currentIndex === 0}">
+    < Prev
+  </button>
+
+  <button @click=${this._nextSlide}
+    ?disabled="${this.currentIndex === this.slides.length - 1}">
+    Next >
+  </button>
   <slot></slot>
 </div>`;
   }
 
+  _nextSlide() {
+    if (this.currentIndex < this.slides.length - 1) {
+      this.currentIndex++;
+    }
+  }
+
+  _prevSlide() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    }
+  }
   /**
    * haxProperties integration via file reference
    */
